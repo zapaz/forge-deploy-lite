35ea9860e30534e0f83e0eba49c98a7d26209814
zapaz.eth <alain@kredeum.com>
2023-09-11 22:15:41 +0200

    new logs and set deployer

--- script/DeployLite.s.sol
+++ script/DeployLite.s.sol
@@ -2,29 +2,60 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "script/ReadWriteJson.s.sol";
-import {IDeployLite} from "script/interfaces/IDeployLite.sol";
-import {ISliceBytes} from "script/interfaces/ISliceBytes.sol";
+import "./ReadWriteJson.s.sol";
+import {IDeployLite} from "./interfaces/IDeployLite.sol";
+import {IUtils} from "./interfaces/IUtils.sol";
 
-contract DeployLite is IDeployLite, ISliceBytes, Script, ReadWriteJson {
+contract DeployLite is Script, IDeployLite, IUtils, ReadWriteJson {
     address deployer;
+
+    function log3(address addr, string memory name, string memory description) public view override(IUtils) {
+        log4(addr, name, description, "");
+    }
+
+    function log4(address addr, string memory name, string memory description, string memory more)
+        public
+        view
+        override(IUtils)
+    {
+        console.log(addr, _stringPad20(name), _stringPad20(description), _stringPad20(more));
+    }
+
+    function getCallerModeName(uint256 callerMode) public pure returns (string memory) {
+        require(callerMode < 5, "Unknown mode");
+        string[5] memory modes = ["None", "Broadcast", "RecurrentBroadcast", "Prank", "RecurrentPrank"];
+        return modes[callerMode];
+    }
+
+    function logCallers(string memory label) public {
+        (VmSafe.CallerMode callerMode, address msgSender, address txOrigin) = vm.readCallers();
+        string memory mode = getCallerModeName(uint256(callerMode));
+
+        if (msgSender == txOrigin) {
+            log4(txOrigin, label, "msgSender and txOrigin", mode);
+        } else {
+            log4(txOrigin, label, "txOrigin", mode);
+            log4(msgSender, label, "msgSender", mode);
+        }
+    }
 
     function deploy(string memory name, bool update) public override(IDeployLite) returns (address addr) {
         if (!_existsJsonFile()) _createJsonFile(block.chainid);
 
-        deployer = getDeployer();
+        if (deployer == address(0)) deployer = msg.sender;
+
         addr = readAddress(name);
         uint256 codeDeployedLength = getCodeDeployed(name).length;
 
         if (isSameDeployed(name)) {
-            console.log("%s Existing     %s %s bytes", addr, _stringPad20(name), _uintPad5(codeDeployedLength));
+            log4(addr, _stringPad20(name), "Already deployed", _bytesPad5(codeDeployedLength));
         } else {
             if (isDeployed(name)) {
-                console.log("%s Old deploy   %s %s bytes", addr, _stringPad20(name), _uintPad5(codeDeployedLength));
+                log4(addr, _stringPad20(name), "Older deployment", _bytesPad5(codeDeployedLength));
 
                 if (!update) return addr;
             }
-            console.log("%s Deploying... %s", addr, name);
+            log4(addr, name, "Deploying...", "");
 
             string memory deployFunction = string.concat("deploy", name, "()");
             (bool success, bytes memory result) = address(this).call(abi.encodeWithSignature(deployFunction));
@@ -34,7 +65,19 @@ contract DeployLite is IDeployLite, ISliceBytes, Script, ReadWriteJson {
 
             writeAddressToCache(name, addr);
 
-            console.log("%s New deploy   %s %s bytes", addr, _stringPad20(name), _uintPad5(addr.code.length));
+            log4(addr, _stringPad20(name), "New deployment", _bytesPad5(addr.code.length));
+        }
+    }
+
+    function getAddress(string memory name) public override(IDeployLite) returns (address addr) {
+        addr = readAddress(name);
+
+        if (addr == address(0)) {
+            addr = makeAddr(name);
+            writeAddressToCache(name, addr);
+            log4(addr, name, "New EOA", "");
+        } else {
+            log4(addr, name, "Existing", "");
         }
     }
 
@@ -62,50 +105,41 @@ contract DeployLite is IDeployLite, ISliceBytes, Script, ReadWriteJson {
         return isSameRunCode(getCodeToDeploy(name), getCodeDeployed(name));
     }
 
-    // get address if exists, creates a fake one otherwise
-    // to not get fake one, use directly readAddress that will return default address(0)
-    function getAddress(string memory name) public override(IDeployLite) returns (address addr) {
-        addr = readAddress(name);
-        if (addr == address(0)) {
-            addr = makeAddr(name);
-            console.log(addr, "New EOA     ", name);
-        } else {
-            addr = msg.sender;
-            console.log(addr, "Existing    ", name);
-        }
+    function setDeployer(address deployer_) public override(IDeployLite) {
+        deployer = deployer_;
     }
 
-    function getDeployer() public view override(IDeployLite) returns (address) {
-        try vm.envAddress("ETH_FROM") returns (address from) {
-            return from;
-        } catch {
-            return msg.sender;
-        }
+    function resetDeployer() public {
+        deployer = address(0);
     }
 
     function sliceBytes(bytes calldata data, uint256 start, uint256 end)
         public
         pure
-        override(ISliceBytes)
+        override(IUtils)
         returns (bytes memory)
     {
         return bytes(data[start:end]);
     }
 
-    function _uintPad5(uint256 n) internal pure returns (string memory) {
+    function _bytesPad5(uint256 n) internal pure returns (string memory) {
         require(n <= 24_576, "Too big Dragon");
         bytes memory b = new bytes(5);
         b[0] = bytes1(n < 10_000 ? 0x20 : uint8(n / 10_000 % 10) + 0x30);
         b[1] = bytes1(n < 1_000 ? 0x20 : uint8(n / 1_000 % 10) + 0x30);
         b[2] = bytes1(n < 100 ? 0x20 : uint8(n / 100 % 10) + 0x30);
         b[3] = bytes1(n < 10 ? 0x20 : uint8(n / 10 % 10) + 0x30);
-        b[4] = bytes1(n < 1 ? 0x20 : uint8(n % 10) + 0x30);
-        return string(b);
+        b[4] = bytes1(uint8(n % 10) + 0x30);
+        return string.concat(string(b), " bytes");
     }
 
     function _stringPad20(string memory input) internal pure returns (string memory output) {
+        return _stringPad(input, 20);
+    }
+
+    function _stringPad(string memory input, uint256 len) internal pure returns (string memory output) {
         bytes memory str = bytes(input);
-        while (str.length < 20) {
+        while (str.length < len) {
             str = abi.encodePacked(str, " ");
         }
         output = string(str);
